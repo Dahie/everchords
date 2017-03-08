1e40100e56c8eae5bc00c5c50a00d9bb8574a227
Daniel Senff <mail@danielsenff.de>
2017-03-08 19:12:40 +0100

    support 2 songbooks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
   def index
     if current_user&.evernote_token
+      @original_songs = enutils.notes(notebook: 'Songbook Originals', limit: 50)
       @songs = enutils.notes(notebook: 'Songbook', limit: 50)
     else
+      @original_songs = []
       @songs = []
     end
   end
