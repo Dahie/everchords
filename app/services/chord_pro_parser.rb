5cc46ad125a6156eb866d55e26fe248455c1607d
Daniel Senff <mail@danielsenff.de>
2017-05-21 12:11:48 +0200

    improve comments parsing

--- app/services/chord_pro_parser.rb
+++ app/services/chord_pro_parser.rb
@@ -42,22 +42,29 @@ class ChordProParser
     html_output.gsub!(/\[(\S+?)\]/, '<kbd>\1</kbd>')
     html_output.gsub!(/\{colb\}/, '<span class="column-break">')
 
-    # reference http://www.onsongapp.com/docs/features/formats/chordpro/
+    # reference http://www.chordpro.org/chordpro/v50.html
     html_output.gsub!(/\{title:([\S\s]+?)\}/, '<h2 class="title">\1</h2>')
     html_output.gsub!(/\{t:([\S\s]+?)\}/, '<h2 class="title">\1</h2>')
     html_output.gsub!(/\{artist:([\S\s]+?)\}/, '<div class="artist">\1</d>')
     html_output.gsub!(/\{a:([\S\s]+?)\}/, '<div class="artist">\1</d>')
     html_output.gsub!(/\{album:([\S\s]+?)\}/, '<div class="album">\1</div>')
     html_output.gsub!(/\{time:([\S\s]+?)\}/, '<div class="time">\1</div>')
+    html_output.gsub!(/\{year:([\S\s]+?)\}/, '<div class="year">\1</div>')
     html_output.gsub!(/\{tempo:([\S\s]+?)\}/, '<div class="tempo">\1</div>')
     html_output.gsub!(/\{capo:([\S\s]+?)\}/, '<div class="capo">\1</div>')
     html_output.gsub!(/\{album:([\S\s]+?)\}/, '<div class="album">\1</div>')
 
 
+    html_output.gsub!(/(#[\s\S]+?$)/, '')
+
     html_output.gsub!(/\{comment:([\S\s]+?)\}/, '<span class="comment text-muted">\1</span>')
     html_output.gsub!(/\{c:([\S\s]+?)\}/, '<span class="comment text-muted">\1</span>')
-    html_output.gsub!(/\{soc\}([\S\s]+?)\{eoc\}/, '<div class="chorus">\1</div>')
-    html_output.gsub!(/\{toc\}([\S\s]+?)\{toc\}/, '<div class="tab">\1</div>')
+    html_output.gsub!(/\{chorus\}/, '<span class="comment text-muted">Chorus</span>')
+    #html_output.gsub!(/\{soc\}([\S\s]+?)\{eoc\}/, '<div class="chorus">\1</div>')
+    html_output.gsub!(/<div>\{start_of_verse\}\s<\/div>([\S\s]+?)<div>\{end_of_verse\}\s<\/div>/, '<div class="verse">\1</div>')
+    html_output.gsub!(/\{sov\}([\S\s]+?)\{eov\}/, '<div class="verse">\1</div>')
+    html_output.gsub!(/<div>\{soc\}\s<\/div>([\S\s]+?)<div>\{eoc\}\s<\/div>/, '<blockquote>\1</blockquote>')
+    html_output.gsub!(/<div>\{start_of_tab\}\s<\/div>([\S\s]+?)<div>\{end_of_tab\}\s<\/div>/, '<div class="tab">\1</div>')
     #substitute_chords(html_output)
     html_output
   end
