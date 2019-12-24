4f62633f1c3f071f2c14e88510325a22e4b68b05
Daniel Senff <mail@danielsenff.de>
2019-12-24 13:11:11 +0100

    add imprint and footer

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
   resources :songs, only: %i[show update]
   resources :notebooks, only: %i[new create update destroy]
+  get 'imprint' => 'pages#imprint', as: 'imprint'
 end
