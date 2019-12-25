17357d65842d4933bc8b4fdc25309a15fa8f2490
Daniel Senff <mail@danielsenff.de>
2019-12-25 08:27:58 +0100

    additional spec tests on song model

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -18,14 +18,45 @@ describe Song do
   describe :scopes do
   end
 
-  subject { create(:song) }
+  subject { build(:song) }
 
-  describe 'set_secret_token' do
+  describe '#set_secret_token' do
     it 'updates token' do
       subject.secret_token = nil
       expect do
         subject.set_secret_token
       end.to change(subject, :secret_token).from(nil)
+    end
+  end
+
+  describe 'share_url' do
+    let(:secret_token) { 'foo' }
+    let(:id) { 42 }
+
+    it 'returns correct url' do
+      subject.secret_token = secret_token
+      subject.id = 42
+      expect(subject.share_url).to eql("/songs/#{id}?secret_token=#{secret_token}")
+    end
+  end
+
+  describe '#update_from_evernote' do
+    let(:content) { 'foo' }
+    let(:title) { 'bar' }
+    let(:evernote_note) do
+      double(:evernote_note, content: content, title: title)
+    end
+
+    it 'updates title' do
+      expect do
+        subject.update_from_evernote(evernote_note)
+      end.to change(subject, :title).to(title)
+    end
+
+    it 'updates body' do
+      expect do
+        subject.update_from_evernote(evernote_note)
+      end.to change(subject, :body).to(content)
     end
   end
 end
