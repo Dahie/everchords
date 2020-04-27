be27ee97a7c15ed614a1db17bf0dc91804019ca8
Daniel Senff <mail@danielsenff.de>
2020-04-27 13:54:10 +0200

    add admin flag on user and use it in UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_085550) do
+ActiveRecord::Schema.define(version: 2020_04_27_115146) do
 
   create_table "notebooks", force: :cascade do |t|
     t.integer "user_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_01_05_085550) do
     t.string "provider"
     t.string "username"
     t.string "avatar"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
