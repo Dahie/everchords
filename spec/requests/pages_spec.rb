1a1344deff300e00531f20d0a8c2d60b794b113b
Daniel Senff <mail@danielsenff.de>
2022-06-22 20:50:57 +0200

    add spec tests for static pages

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe "Pages", type: :request do
+  
+  %i[help imprint].each do |page|
+    describe "GET /#{page}" do
+      it do
+        get("/#{page}")
+        expect(response).to render_template(page)
+      end
+    end
+  end
+end
