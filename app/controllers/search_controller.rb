f5631d7259866d7c1cb07ac66fbbae15842e2d96
Daniel Senff <mail@danielsenff.de>
2022-07-03 10:54:03 +0200

    refactor search controller to suit linting

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,32 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
   def show
     render(plain: 'Not Authorized', status: :unauthorized) and return true unless current_user
 
-    results = {}
-    current_user.notebooks.each do |notebook|
-      results[notebook.id] = { name: notebook.name, results: notebook.songs.where('lower(title) LIKE ?', "%#{params[:query].downcase}%").map do |song|
-                                                               { title: song.title, url: user_song_path(song.user, song) }
-                                                             end }
-    end
-
     body = {
-      results:
+      results: search_results(params[:query])
     }
     render json: body.to_json
+  end
+
+  private
+
+  def search_results(query)
+    {}.tap do |results|
+      current_user.notebooks.each do |notebook|
+        results[notebook.id] = {
+          name: notebook.name,
+          results: notebook_results(query, notebook)
+        }
+      end
+    end
+  end
+
+  def notebook_results(query, notebook)
+    notebook.songs.where('lower(title) LIKE ?', "%#{query.downcase}%").map do |song|
+      { title: song.title,
+        url: user_song_path(song.user, song) }
+    end
   end
 end
