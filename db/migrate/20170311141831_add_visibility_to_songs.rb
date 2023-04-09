969604605e049e6a56a84d4e9878c60946ca4fcf
Daniel Senff <mail@danielsenff.de>
2023-04-09 08:17:23 +0200

    fix rubocop linting

--- db/migrate/20170311141831_add_visibility_to_songs.rb
+++ db/migrate/20170311141831_add_visibility_to_songs.rb
@@ -3,7 +3,7 @@
 class AddVisibilityToSongs < ActiveRecord::Migration[5.0]
   def change
     change_table :songs, bulk: true do |t|
-      t.boolean :published, default: false
+      t.boolean :published, default: false, null: false
       t.string :secret_token
     end
   end
--- db/migrate/20200427115146_add_admin_flag_to_users.rb
+++ db/migrate/20200427115146_add_admin_flag_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddAdminFlagToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :admin, :boolean, default: false
+    add_column :users, :admin, :boolean, default: false, null: false
   end
 end
